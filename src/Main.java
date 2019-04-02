8e897037c987b1ee75c337f7c49ed7ce3efdca03
mgpeklund <mgp.eklund@gmail.com>
2019-04-02 20:26:11 +0200

    Added stub for main method (and some comments)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    /**
+     * Main method for game, instantiate and execute game from here.
+     * This is a comment that describes the main method of the program.
+     *
+     * @param args argument description goes here
+     */
+    public static void main(String[] args) {
+
+        // This is also a comment
+    }
 }
