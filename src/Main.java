a157a9da8532fbf53adb55cc1c66bccbc8146bb9
meklund <mgpeklund@gmail.com>
2019-04-05 19:19:04 +0200

    loops, one broken.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,45 @@ public class Main {
      * @param args argument description goes here
      */
     public static void main(String[] args) {
+        boolean condition;                      // instantiate a boolean
+        int[] list_of_numbers = {19,14,13,42};  // instantiate a list of ints
 
-        // This is a test
+        /*
+         * For loop
+         */
+        System.out.println("=== For-loop");
+        for (int i = 0; i < list_of_numbers.length; i++){
+            System.out.println(list_of_numbers[i]);
+        }
+
+        /*
+        foreach loop
+         */
+        System.out.println("\n=== foreach loop");
+        for (int i: list_of_numbers) {
+            System.out.println(i);
+        }
+
+        /*
+        while loop
+         */
+        System.out.println("\n=== while loop");
+        int i = 0;
+        condition = true;
+        while (condition){
+            System.out.println(list_of_numbers[i++]);
+
+            condition = i < list_of_numbers.length;
+        }
+
+        /*
+        do-while loop
+         */
+        System.out.println("\n=== do-while loop");
+        do {
+            System.out.println(list_of_numbers[i++]);
+
+            condition = i < list_of_numbers.length;
+        } while (condition);
     }
 }
